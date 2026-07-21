5deca8aa07fc438bd13b904aa627b2ad1a8ffadf
Erika <erikapenullar@gmail.com>
2026-07-20 23:24:59 -1000

    Delete next.config.mjs

--- next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    unoptimized: true, // Required for static GitHub Pages export if using <Image> tags
-  },
-};
-
-export default nextConfig;
